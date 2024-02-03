552a5a621af2bd21f97c6ebed60af299c8b66d61
Isak Horvath <isakhorvath@gmail.com>
2024-02-03 12:15:33 +0100

    Mixer: Improve vertex selection

--- cube.vert
+++ cube.vert
@@ -5,8 +5,10 @@ in  vec3 in_Color;
 
 uniform mat4 rotationMatrix;
 uniform mat4 rotationMatrix2;
-uniform mat4 translationMatrix;
+uniform mat4 mdlMatrix;
 uniform mat4 projMatrix;
+
+uniform mat4 camMatrix;
 
 uniform bool useUniformColor;
 uniform vec3 uniformColor;
@@ -15,7 +17,7 @@ out  vec3 color;
 
 void main(void)
 {
-	gl_Position = projMatrix * translationMatrix * rotationMatrix * rotationMatrix2 * vec4(in_Position, 1.0);
+	gl_Position = projMatrix * camMatrix * mdlMatrix * vec4(in_Position, 1.0);
 
     if (useUniformColor) {
         color = uniformColor;
--- main.cpp
+++ main.cpp
@@ -59,26 +59,12 @@ GLubyte lineIndices[8*3] = {
     0, 4, 1, 5, 2, 6, 3, 7  // Side edges
 };
 
-GLfloat rotationMatrix[] = {
-	1.0f, 0.0f, 0.0f, 0.0f,
-	0.0f, 1.0f, 0.0f, 0.0f,
-	0.0f, 0.0f, 1.0f, 0.0f,
-	0.0f, 0.0f, 0.0f, 1.0f 
-};
-
-GLfloat rotationMatrix2[] = {	
-	1.0f, 0.0f, 0.0f, 0.0f,
-	0.0f, 1.0f, 0.0f, 0.0f,
-	0.0f, 0.0f, 1.0f, 0.0f,
-	0.0f, 0.0f, 0.0f, 1.0f 
-};
-
-GLfloat translationMatrix[] = {	
+mat4 translationMatrix = mat4(	
 	1.0f, 0.0f, 0.0f, 0.0f,
 	0.0f, 1.0f, 0.0f, 0.0f,
 	0.0f, 0.0f, 1.0f, -2.0f,
 	0.0f, 0.0f, 0.0f, 1.0f 
-};
+);
 
 #define near 1.0
 #define far 30.0
@@ -86,15 +72,32 @@ GLfloat translationMatrix[] = {
 #define left -1.0
 #define top 1.0
 #define bottom -1.0
-GLfloat projectionMatrix[] = {	
-	2.0f*near/(right-left), 0.0f, (right+left)/(right-left), 0.0f,
-	0.0f, 2.0f*near/(top-bottom), (top+bottom)/(top-bottom), 0.0f,
-	0.0f, 0.0f, -(far + near)/(far - near), -2*far*near/(far - near),
-	0.0f, 0.0f, -1.0f, 0.0f 
-};
+mat4 projectionMatrix = mat4(
+    2.0f * near / (right - left), 0.0f, (right + left) / (right - left), 0.0f,
+    0.0f, 2.0f * near / (top - bottom), (top + bottom) / (top - bottom), 0.0f,
+    0.0f, 0.0f, -(far + near) / (far - near), -2 * far * near / (far - near),
+    0.0f, 0.0f, -1.0f, 0.0f
+);
+
+mat4 viewToWorld = lookAtv(
+	vec3(0.0f, 0.0f, 0.5f), 
+	vec3(0.0f, 0.0f, 0.0f),
+	vec3(0.0f, 1.0f, 0.0f)
+);
+
+mat4 rotationMatrix1, rotationMatrix2, rotationMatrix3;
+mat4 mdlMatrix;
 
 unsigned int vertexArrayObjID;
 GLuint program;
+
+float generateRandomFloat(float range) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-range, range);
+    
+    return dis(gen);
+}
 
 unsigned int indexBufferObjID;
 unsigned int lineIndexBufferObjID;
@@ -137,9 +140,15 @@ void init(void)
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lineIndexBufferObjID);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(lineIndices), lineIndices, GL_STATIC_DRAW);
 
-	glUniformMatrix4fv(glGetUniformLocation(program, "translationMatrix"), 1, GL_TRUE, translationMatrix);
-	glUniformMatrix4fv(glGetUniformLocation(program, "rotationMatrix"), 1, GL_TRUE, rotationMatrix);
-	glUniformMatrix4fv(glGetUniformLocation(program, "projMatrix"), 1, GL_TRUE, projectionMatrix);
+	glUniformMatrix4fv(glGetUniformLocation(program, "camMatrix"), 1, GL_TRUE, viewToWorld.m);
+
+	rotationMatrix1 = Rx(generateRandomFloat(180.0f));
+	rotationMatrix2 = Ry(generateRandomFloat(180.0f));
+	rotationMatrix3 = Rz(generateRandomFloat(180.0f));
+	mdlMatrix = translationMatrix * rotationMatrix1 * rotationMatrix2 * rotationMatrix3;
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, mdlMatrix.m);
+	//glUniformMatrix4fv(glGetUniformLocation(program, "rotationMatrix"), 1, GL_TRUE, rotationMatrix);
+	glUniformMatrix4fv(glGetUniformLocation(program, "projMatrix"), 1, GL_TRUE, projectionMatrix.m);
 
 	printError("init arrays");
 }
@@ -172,22 +181,14 @@ void updateVertexPosition(int vertexIndex, float x, float y, float z) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
-float generateRandomFloat(float range) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(-range, range);
-    
-    return dis(gen);
-}
-
 void updateVertexPositions() {
 	for(int i = 0; i < 8; i++) {
 		float currentX, currentY, currentZ;
 		getCurrentVertexPosition(i, &currentX, &currentY, &currentZ);
 
-		float randomX = currentX + generateRandomFloat(0.0025f);
-		float randomY = currentY + generateRandomFloat(0.0025f);
-		float randomZ = currentZ + generateRandomFloat(0.0025f);
+		float randomX = currentX + generateRandomFloat(0.005f);
+		float randomY = currentY + generateRandomFloat(0.005f);
+		float randomZ = currentZ + generateRandomFloat(0.005f);
 
 		updateVertexPosition(i, randomX, randomY, randomZ);
 	}
@@ -201,11 +202,12 @@ void display(void)
 {
 	updateVertexPositions(); //debug purposes
     printError("pre display");
-    angle += speed;
+    // angle += speed;
 
     // clear screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	/*
     rotationMatrix2[0] = cos(angle/5.0);
     rotationMatrix2[1] = -sin(angle/5.0);
     rotationMatrix2[4] = sin(angle/5.0);
@@ -216,6 +218,7 @@ void display(void)
     rotationMatrix[9] = sin(angle);
     rotationMatrix[10] = cos(angle);
     glUniformMatrix4fv(glGetUniformLocation(program, "rotationMatrix"), 1, GL_TRUE, rotationMatrix);
+	*/
 
     glBindVertexArray(vertexArrayObjID);
     
@@ -253,35 +256,47 @@ void display(void)
     glutSwapBuffers();
 }
 
-void mouse(int button, int state, int x, int y) { // TODO: this function does not work as supposed to
+void screenToWorldCoordinates(float screenX, float screenY, float& worldX, float& worldY, float& worldZ) {
+    float screenWidth = glutGet(GLUT_WINDOW_WIDTH);
+    float screenHeight = glutGet(GLUT_WINDOW_HEIGHT);
+
+    float ndcX = (screenX / screenWidth) * 2.0f - 1.0f;
+    float ndcY = 1.0f - (screenY / screenHeight) * 2.0f;
+
+    vec4 ndcPos = vec4(ndcX, ndcY, -1.0f, 1.0f);
+    vec4 clipPos = inverse(projectionMatrix) * ndcPos;
+    vec4 worldPos = inverse(viewToWorld) * clipPos;
+
+    worldX = worldPos.x / worldPos.w;
+    worldY = worldPos.y / worldPos.w;
+    worldZ = worldPos.z / worldPos.w;
+}
+
+void mouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        float screenWidth = glutGet(GLUT_WINDOW_WIDTH);
-        float screenHeight = glutGet(GLUT_WINDOW_HEIGHT);
-        
-        float ndcX = (x / screenWidth) * 2.0f - 1.0f;
-        float ndcY = 1.0f - (y / screenHeight) * 2.0f;
+        float worldX, worldY, worldZ;
+        screenToWorldCoordinates(x, y, worldX, worldY, worldZ);
 
-        float clickedWorldX = ndcX;
-        float clickedWorldY = ndcY;
-        float clickedWorldZ = 0;
-		
-        float minDistance = std::numeric_limits<float>::max();
+        float closestDistance = std::numeric_limits<float>::max();
         int closestVertexIndex = -1;
-
         for (int i = 0; i < 8; ++i) {
-            float vertexX, vertexY, vertexZ;
-            getCurrentVertexPosition(i, &vertexX, &vertexY, &vertexZ);
-            float distance = sqrt(pow(vertexX - clickedWorldX, 2) + pow(vertexY - clickedWorldY, 2) + pow(vertexZ - clickedWorldZ, 2));
-            if (distance < minDistance) {
-                minDistance = distance;
+            float vertexWorldX, vertexWorldY, vertexWorldZ;
+            getCurrentVertexPosition(i, &vertexWorldX, &vertexWorldY, &vertexWorldZ);
+
+            vec4 vertexPos = vec4(vertexWorldX, vertexWorldY, vertexWorldZ, 1.0f);
+            vertexPos = mdlMatrix * vertexPos;
+
+            float distance = sqrt(pow(vertexPos.x - worldX, 2) + pow(vertexPos.y - worldY, 2) + pow(vertexPos.z - worldZ, 2));
+            if (distance < closestDistance) {
+                closestDistance = distance;
                 closestVertexIndex = i;
             }
         }
 
         selectedVertexIndex = closestVertexIndex;
+        glutPostRedisplay();
     }
 }
-
 
 int main(int argc, char *argv[])
 {
